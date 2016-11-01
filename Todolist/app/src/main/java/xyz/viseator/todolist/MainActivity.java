b46c27d8ec6e72c575f3e2f69b39ad27990377dd
viseator <viseator@gmail.com>
2016-11-01 10:59:11 +0800

    layout step 1

--- Todolist/app/src/main/java/xyz/viseator/todolist/MainActivity.java
+++ Todolist/app/src/main/java/xyz/viseator/todolist/MainActivity.java
@@ -1,13 +1,21 @@
 package xyz.viseator.todolist;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 }
+
