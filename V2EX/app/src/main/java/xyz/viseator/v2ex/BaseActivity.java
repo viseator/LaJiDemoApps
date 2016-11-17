939bd02f0f2ad73fa429c1c36e20d0ba47b0425b
viseator <viseator@gmail.com>
2016-11-17 11:30:56 +0800

    V2EX init View

---
+++ V2EX/app/src/main/java/xyz/viseator/v2ex/BaseActivity.java
@@ -0,0 +1,10 @@
+package xyz.viseator.v2ex;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by viseator on 2016/11/17.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+}
---
+++ V2EX/app/src/main/java/xyz/viseator/v2ex/activities/MainActivity.java
@@ -0,0 +1,19 @@
+package xyz.viseator.v2ex.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import xyz.viseator.v2ex.BaseActivity;
+
+/**
+ * Created by viseator on 2016/11/17.
+ */
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
