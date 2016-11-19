f1d8b6d6f77899c0bc4d4b26eb3f573b0c7bdd85
viseator <viseator@gmail.com>
2016-11-19 16:56:11 +0800

    show start ok

---
+++ V2EX/app/src/main/java/xyz/viseator/v2ex/http/GetAvatarTask.java
@@ -0,0 +1,49 @@
+package xyz.viseator.v2ex.http;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by viseator on 2016/11/19.
+ */
+
+public class GetAvatarTask extends AsyncTask<String, Void, Bitmap> {
+
+    private ImageView imageView;
+    private final static String TAG = "wudi GetAvatarTask";
+
+    public GetAvatarTask(ImageView imageView) {
+        this.imageView = imageView;
+    }
+
+    @Override
+    protected Bitmap doInBackground(String... strings) {
+        String url = "http:" + strings[0];
+        Bitmap bitmap;
+        try {
+            URL url1 = new URL(url);
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url1.openConnection();
+            InputStream inputStream = httpURLConnection.getInputStream();
+            bitmap = BitmapFactory.decodeStream(inputStream);
+            inputStream.close();
+            return bitmap;
+        } catch (IOException e) {
+            Log.d(TAG, "URL error");
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Bitmap bitmap) {
+        imageView.setImageBitmap(bitmap);
+    }
+}
